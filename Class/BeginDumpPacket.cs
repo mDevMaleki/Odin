fba76b85dbf87e1ab2e410f69bb3321513ae2a62
mdevmaleki <mdevmaleki@gmail.com>
2023-03-27 23:13:42 +0430

    add filetranferpacket beginsessionpacket setupsettionpacket to project

--- Class/BeginDumpPacket.cs
+++ Class/BeginDumpPacket.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace OdinProtocol.Class
 {
-    internal class BeginDumpPacket
+    public enum kChipType
     {
+        Ram = 0,
+        Nand = 1
+    };
+
+    internal class BeginDumpPacket : FileTransferPacket
+    {
+        private uint chipType;
+        private uint chipId;
+
+        public uint ChipType { get { return chipType; } }
+        public uint ChipId { get { return chipId; } }
+
+        public BeginDumpPacket(uint chipType, uint chipId) : base(Convert.ToUInt32(kRequest.Dump))
+        {
+            this.chipType = chipType;
+            this.chipId = chipId;
+        }
+
+
+        protected override void Pack()
+        {
+            base.Pack();
+            PackInteger(kDataSize, chipType);
+            PackInteger(kDataSize + 4, chipId);
+        }
     }
 }
--- Class/ControlPacket.cs
+++ Class/ControlPacket.cs
@@ -1,23 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace OdinProtocol.Class
 {
 
     public enum kControlType
     {
-        kControlTypeSession = 0x64,
-        kControlTypePitFile = 0x65,
-        kControlTypeFileTransfer = 0x66,
-        kControlTypeEndSession = 0x67
+        Session = 0x64,
+        PitFile = 0x65,
+        FileTransfer = 0x66,
+        EndSession = 0x67
     };
 
     internal class ControlPacket : OutboundPacket
     {
-        protected const int kDataSize = 4;
+        protected int kDataSize { get { return 4; } }
+       
         public ControlPacket(uint controlType) : base(1024)
         {
             this.controlType = controlType;
