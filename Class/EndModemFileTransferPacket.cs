08b742948730aafd5faefa182bf42257f9002e7b
mdevmaleki <mdevmaleki@gmail.com>
2023-03-29 12:34:05 +0430

    add endmodempacket endfilepackets to project

---
+++ Class/EndModemFileTransferPacket.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdinProtocol.Class
+{
+    internal class EndModemFileTransferPacket : EndFileTransferPacket
+    {
+        private uint endOfFile;
+
+        public bool IsEndOfFile { get { return (endOfFile == 1); } }
+        public EndModemFileTransferPacket(uint sequenceByteCount, uint unknown1, uint chipIdentifier,  bool endOfFile) : base(Convert.ToUInt32(kDestination.Modem), sequenceByteCount, unknown1, chipIdentifier)
+        {
+            this.endOfFile = (uint)(endOfFile ? 1 : 0);
+        }
+
+        protected override void Pack()
+        {
+            base.Pack();
+
+            PackInteger(base.kDataSize, endOfFile);
+        }
+
+    }
+}
