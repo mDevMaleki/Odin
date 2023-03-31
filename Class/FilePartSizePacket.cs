72548fcb90d6145ef632490014b93836952f40e7
mdevmaleki <mdevmaleki@gmail.com>
2023-03-31 23:54:44 +0430

    add filePartSizePacket to project

---
+++ Class/FilePartSizePacket.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdinProtocol.Class
+{
+    internal class FilePartSizePacket : SessionSetupPacket
+    {
+
+        private uint filePartSize;
+
+        public uint FilePartSize { get { return filePartSize; } }
+        public FilePartSizePacket(uint filePartSize) : base(Convert.ToUInt32(SessionType.kFilePartSize))
+        {
+            this.filePartSize = filePartSize;
+        }
+
+
+        protected override void Pack()
+        {
+            base.Pack();
+
+            PackInteger(kDataSize, filePartSize);
+        }
+    }
+}
