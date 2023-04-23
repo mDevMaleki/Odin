548157ab80b83ab69f88355e61edb65390390daf
mdevmaleki <mdevmaleki@gmail.com>
2023-04-23 12:53:41 +0430

    add ReceiveFilePartPacket to classes

---
+++ Class/ReceiveFilePartPacket.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdinProtocol.Class
+{
+    internal class ReceiveFilePartPacket : InboundPacket
+    {
+        //ToDo
+        //private uint kDataSize = 500;
+      
+
+        public ReceiveFilePartPacket() : base(500, true)
+        {
+        }
+
+        protected override bool Unpack()
+        {
+            return (true);
+        }
+    }
+}
